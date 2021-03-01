7048c0f9031e3a8322abfcc7ba3b4e78a694b0c3
Aleksandr <a@petrov.host>
2021-03-01 17:46:17 +0300

    remove ios unused

--- src/ios/FirebaseVisionPlugin.swift
+++ src/ios/FirebaseVisionPlugin.swift
@@ -4,33 +4,6 @@ import UIKit
 @objc(FirebaseVisionPlugin)
 class FirebaseVisionPlugin: CDVPlugin {
 
-    @objc(onDeviceTextRecognizer:)
-    func onDeviceTextRecognizer(command: CDVInvokedUrlCommand) {
-        guard let imageURL = command.arguments.first as? String else {
-            let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Image URL required")
-            self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
-            return
-        }
-        getImage(imageURL: imageURL) { (image, error) in
-            if let error = error {
-                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: error.localizedDescription)
-                self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
-            } else {
-                let textRecognizer = TextRecognizer.textRecognizer()
-                let visionImage = VisionImage(image: image!)
-                textRecognizer.process(visionImage) { (text, error) in
-                    if let error = error {
-                        let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: error.localizedDescription)
-                        self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
-                    } else {
-                        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: text?.toJSON(with: image!))
-                        self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
-                    }
-                }
-            }
-        }
-    }
-
     @objc(barcodeDetector:)
     func barcodeDetector(command: CDVInvokedUrlCommand) {
         guard let imageURL = command.arguments.first as? String else {
@@ -52,36 +25,6 @@ class FirebaseVisionPlugin: CDVPlugin {
                     } else {
                         let barcodesDict = barcodes?.compactMap({ $0.toJSON(with: image!) })
                         let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: barcodesDict)
-                        self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
-                    }
-                }
-            }
-        }
-    }
-
-    @objc(imageLabeler:)
-    func imageLabeler(command: CDVInvokedUrlCommand) {
-        guard let imageURL = command.arguments.first as? String else {
-            let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Image URL required")
-            self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
-            return
-        }
-        getImage(imageURL: imageURL) { (image, error) in
-            if let error = error {
-                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: error.localizedDescription)
-                self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
-            } else {
-                let options = ImageLabelerOptions()
-                options.confidenceThreshold = 0.7
-                let imageLabeler = ImageLabeler.imageLabeler(options: options)
-                let visionImage = VisionImage(image: image!)
-                imageLabeler.process(visionImage) { (labels, error) in
-                    if let error = error {
-                        let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: error.localizedDescription)
-                        self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
-                    } else {
-                        let labelsDict = labels?.compactMap({$0.toJSON()})
-                        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: labelsDict)
                         self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
                     }
                 }
