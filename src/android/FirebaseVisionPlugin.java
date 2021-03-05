8f3cbf8a43ce0cb08deb88a98edc498a116a4df4
Aleksandr <a@petrov.host>
2021-03-05 12:35:47 +0300

    remove zbar and revert prevois retun type

--- src/android/FirebaseVisionPlugin.java
+++ src/android/FirebaseVisionPlugin.java
@@ -15,11 +15,8 @@ import com.google.mlkit.vision.barcode.BarcodeScanning;
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
 import com.google.mlkit.vision.common.InputImage;
 
-import net.sourceforge.zbar.ImageScanner;
-import net.sourceforge.zbar.Image;
-import net.sourceforge.zbar.Symbol;
-import net.sourceforge.zbar.SymbolSet;
-import net.sourceforge.zbar.Config;
+
+import android.util.Log;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -62,39 +59,41 @@ public class FirebaseVisionPlugin extends CordovaPlugin {
             try {
                 //InputImage image = getImage(message);
 
-                ImageScanner ZBarScanner = new ImageScanner();
-                ZBarScanner.setConfig(0, Config.X_DENSITY, 3);
-                ZBarScanner.setConfig(0, Config.Y_DENSITY, 3);
-                ZBarScanner.setConfig(64, Config.ENABLE, 1); // 64 only QR code
+                //ImageScanner ZBarScanner = new ImageScanner();
+                //ZBarScanner.setConfig(0, Config.X_DENSITY, 3);
+                //ZBarScanner.setConfig(0, Config.Y_DENSITY, 3);
+                //ZBarScanner.setConfig(64, Config.ENABLE, 1); // 64 only QR code
 
 
                 byte[] base64data = base64toByte(message, callbackContext);
                 Bitmap bitMap = BitmapFactory.decodeByteArray(base64data, 0, base64data.length);
                 BarcodeScannerOptions options =
-                                        new BarcodeScannerOptions.Builder()
-                                        .setBarcodeFormats(
-                                                Barcode.FORMAT_QR_CODE,
-                                                Barcode.FORMAT_AZTEC
-                                        )
-                                        .build();
+                        new BarcodeScannerOptions.Builder()
+                                .setBarcodeFormats(
+                                        Barcode.FORMAT_QR_CODE,
+                                        Barcode.FORMAT_AZTEC
+                                )
+                                .build();
                 BarcodeScanner detector = BarcodeScanning.getClient(options);
 
                 InputImage image = InputImage.fromBitmap(bitMap, 0);
 
-                Image barcode = new Image(bitMap.getWidth(), bitMap.getHeight(), "Y800");
-                barcode.setData(base64data);
+                //Image barcode = new Image(bitMap.getWidth(), bitMap.getHeight(), "Y800");
+                //barcode.setData(base64data);
 
-                if (ZBarScanner.scanImage(barcode) != 0) {
+                /*if (ZBarScanner.scanImage(barcode) != 0) {
                     SymbolSet syms = ZBarScanner.getResults();
                     String qrValue = "";
+
                     for (Symbol sym : syms) {
+                        Log.d("MyLogTag!!!!!!!!!", "Found in zbar");
                         qrValue = sym.getData();
 
                         // Return 1st found QR code value to the calling Activity.
                     }
                     callbackContext.success(qrValue);
                     return;
-                }
+                }*/
 
                 detector.process(image)
                         .addOnSuccessListener(new OnSuccessListener<List<Barcode>>() {
@@ -109,43 +108,43 @@ public class FirebaseVisionPlugin extends CordovaPlugin {
                             }
                         })
                         .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                callbackContext.error(e.getLocalizedMessage());
-                            }
-                        });
-            } catch (Exception e) {
-                callbackContext.error(e.getLocalizedMessage());
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    callbackContext.error(e.getLocalizedMessage());
+                                }
+                            });
+                } catch (Exception e) {
+                    callbackContext.error(e.getLocalizedMessage());
+                }
+            } else {
+                callbackContext.error("Expected one non-empty string argument.");
             }
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
         }
-    }
 
-    private InputImage getImage(String message) throws IOException {
-        if (message.contains("data:")) {
-            message = message
-                    .replace("data:image/png;base64,", "")
-                    .replace("data:image/jpeg;base64,", "");
-            byte[] decodedString = Base64.decode(message, Base64.DEFAULT);
-            Bitmap bitMap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
-            InputImage image = InputImage.fromBitmap(bitMap, 0);
-            return image;
-        } else {
-            Uri uri = Uri.parse(message);
-            InputImage image = InputImage.fromFilePath(applicationContext, uri);
-            return image;
+        private InputImage getImage(String message) throws IOException {
+            if (message.contains("data:")) {
+                message = message
+                        .replace("data:image/png;base64,", "")
+                        .replace("data:image/jpeg;base64,", "");
+                byte[] decodedString = Base64.decode(message, Base64.DEFAULT);
+                Bitmap bitMap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+                InputImage image = InputImage.fromBitmap(bitMap, 0);
+                return image;
+            } else {
+                Uri uri = Uri.parse(message);
+                InputImage image = InputImage.fromFilePath(applicationContext, uri);
+                return image;
+            }
         }
-    }
 
-    private byte[] base64toByte(String message, CallbackContext callbackContext) throws IOException {
-        byte[] decodedString;
-        if (message.contains("data:")) {
-            message = message.replace("data:image/png;base64,", "").replace("data:image/jpeg;base64,", "");
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
+        private byte[] base64toByte(String message, CallbackContext callbackContext) throws IOException {
+            byte[] decodedString;
+            if (message.contains("data:")) {
+                message = message.replace("data:image/png;base64,", "").replace("data:image/jpeg;base64,", "");
+            } else {
+                callbackContext.error("Expected one non-empty string argument.");
+            }
+            decodedString = Base64.decode(message, Base64.DEFAULT);
+            return decodedString;
         }
-        decodedString = Base64.decode(message, Base64.DEFAULT);
-        return decodedString;
     }
-}
